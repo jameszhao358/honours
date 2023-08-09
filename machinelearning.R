651a440ef9e101b00e2674d3be9ad574503fdc90
James Zhao <jameszhao358@gmail.com>
2023-08-10 05:11:04 +1000

    Reconfigure

--- machinelearning.R
+++ machinelearning.R
@@ -1,17 +1,25 @@
+# Import libraries 
+
 library(tidyr)
 library(dplyr)
 library(factoextra)
+library(dbscan)
+library(ppclust)
+library(e1071)
 
-master_df <- read.csv("C:/Users/james/Desktop/honours/master.csv") %>%
+# Select significant variables 
+
+features_temp <- read.csv("C:/Users/james/Desktop/honours/processed_data/extracted_features.csv")
+
+features <- features_temp %>%
+  select(overall_kht, transferq_kht, transferq_ft, VVR_duration, transfer_ft, behavioural_ft, non_behavioural_kht, PHQ_classification) %>%
   mutate(PHQ_classification = ifelse(PHQ_classification == "Subclinical", 0, 1)) %>%
-  select(-PIN) %>%
   mutate(across(everything(), ~ replace_na(., median(., na.rm = TRUE)))) # Replacing all n/a values with the column median
 
-data <- master_df %>%
-  select(-"PHQ_classification") %>%
-  select(-c(1:21))
+data <- features %>%
+  select(-PHQ_classification)
 
-data_scale <- scale(data)
+data_scale <- as.data.frame(scale(data))
 
 data <- dist(data_scale)
 
@@ -25,13 +33,11 @@ km.out <- kmeans(data_scale, centers = 3, nstart = 100)
 print(km.out)
 
 km.clusters <- km.out$cluster
-rownames(data_scale) <- paste(master_df$PHQ_classification, 1:dim(master_df)[1], sep = "_")
+rownames(data_scale) <- paste(features$PHQ_classification, 1:dim(features)[1], sep = "_")
 fviz_cluster(list(data=data_scale, cluster = km.clusters))
-table(km.clusters, master_df$PHQ_classification)
+table(km.clusters, features$PHQ_classification)
 
 # DBSCAN clustering 
-
-library(dbscan)
 
 # Finding optimal epsilon using elbow plot
 kNNdistplot(data_scale, minPts = 7)
@@ -43,12 +49,11 @@ plot(data_scale, col = dbscan_res$cluster+1, main = "DBSCAN")
 
 # Fuzzy clustering
 
-library(ppclust)
+fuzzy_clusters <- fcm(data, centers = 3)
 
-fuzzy_clusters <- fcm(data_scale, centers = 3)
-factoextra::fviz_cluster(list(data=fuzzy_clusters, cluster = fuzzy_clusters$cluster),  
+factoextra::fviz_cluster(list(data = fuzzy_clusters, cluster = fuzzy_clusters$cluster),  
                          geom = "point", 
-                         ellipse= FALSE, 
+                         ellipse = FALSE, 
                          show.clust.cent = FALSE,
                          palette = "jco", 
                          ggtheme = theme_classic())
@@ -58,7 +63,4 @@ factoextra::fviz_cluster(list(data=fuzzy_clusters, cluster = fuzzy_clusters$clus
 pca <- prcomp(data_scale)
 fviz_pca_biplot(pca,
                 label="var",
-                habillage = master_df$PHQ_classification)
-
-
-
+                habillage = features$PHQ_classification)
